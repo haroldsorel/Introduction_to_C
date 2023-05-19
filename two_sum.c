093e9761e8489675d6e9fca30ac012f066cc6940
Harold Sorel <133993985+haroldsorel@users.noreply.github.com>
2023-05-19 18:12:07 +0200

    Create two_sum.c
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

---
+++ two_sum.c
@@ -0,0 +1,44 @@
+#include <iostream>
+
+void printnum(int nums[])
+{
+    for( int i = 0; i < 2; i++) {printf("%d\n", nums[i]);}
+}
+
+int* twoSum(int* nums, int target, int size)
+	{
+	    int res[2], elem1;
+	    int i1 = 0; int i2 =0; int test = 0;
+
+	    while (test == 0)
+	    {
+	        res[0] = i1;
+	        elem1 = nums[i1];
+
+       while (i2 != size - 1)
+	        {
+	            if (i1 != i2)
+	            {
+	                if (elem1 + nums[i2] == target)
+	                {
+	                    res[1] = i2;
+	                    test = 1;
+
+	                }
+	            }
+	            i2++;
+	        }
+	        i1++;
+	    }
+        printnum(res);
+	    return res;
+
+	}
+    
+    int main()
+	{
+	    int nums[] = {3,2,4};
+        int size = sizeof(nums)/4;
+	    int target = 9;
+	    twoSum(nums, target, size);
+	}
